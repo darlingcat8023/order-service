4cc41aaf4b2cf322a7f6f9162e146a8ba53dc882
小温柔 <liujiajun8023@gmail.com>
2023-07-05 12:30:38 +0800

    Revert "update"
    This reverts commit 6c1f58baa691876a7961cc7f5cf7643580988517.

--- order-service-charge/src/main/java/com/star/enterprise/order/charge/data/jpa/ChargePropertyLimitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.star.enterprise.order.charge.data.jpa;
-
-import com.star.enterprise.order.charge.data.jpa.entity.ChargePropertyLimitEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface ChargePropertyLimitRepository extends JpaRepository<ChargePropertyLimitEntity, Long>, QuerydslPredicateExecutor<ChargePropertyLimitEntity> {
-
-}
--- order-service-message/src/main/java/com/star/enterprise/order/message/sender/DelayMessageNotActiveException.java
+++
@@ -1,9 +0,0 @@
-package com.star.enterprise.order.message.sender;
-
-/**
- * @author xiaowenrou
- * @date 2023/1/31
- */
-public class DelayMessageNotActiveException extends RuntimeException {
-
-}
